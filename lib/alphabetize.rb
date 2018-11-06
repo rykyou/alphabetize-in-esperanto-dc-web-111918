8bc821f18340599a11c46c61b5fac08c02239700
Robin Kim <rkim1214@gmail.com>
2018-11-06 04:01:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" #constant
+
+def alphabetize(array_of_strings)
+  array_of_strings.sort_by do |string|
+    string.split('').map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end 
+  end
 end
