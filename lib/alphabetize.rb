8aa506197d8561e1190795331e66404622acd9fd
Robin Kim <rkim1214@gmail.com>
2018-11-06 03:59:47 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" #constant
 
 def alphabetize(array_of_strings)
   array_of_strings.sort_by |string|
-  
+    string.split('').map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end 
   end
 end
